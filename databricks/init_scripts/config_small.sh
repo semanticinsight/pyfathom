6562882bc000cae9f8f425529200527168ccde45
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-03-07 18:46:57 +0000

    feature: databricks build

---
+++ databricks/init_scripts/config_small.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#!BEGIN_LIBS
+PYFATHOM="pyfathom"
+#!END_LIBS
+
+WHEELDIR="/dbfs/FileStore/deployment/wheels/"
+
+
+export ENVIRONMENT=DEV
+export AZUREADID=a69c8df4-e648-4b0a-beb9-b3716a01f60e
+export LOGAPPALERTEMAIL=shaun_chibru@hotmail.com
+export AUTOMATIONSCOPE=azure-key-vault-scope
+export STORAGE=AzureDataLakeGen2
+export STORAGEACCOUNT=abfss://datalake@datalakegeneva.dfs.core.windows.net/
+export RESOURCEGROUP=DataPlatform
+export SUBSCRIPTIONID=e95203c2-64a0-43f9-bfc5-a4fdc588571a
+export LOGAPPALERTENDPOINT=LOGAPP-ALERT-ENDPOINT
+export DATAPLATFORMAPPID=DATALAKE-SPN-APPID
+export DATAPLATFORMSECRET=DATALAKE-SPN-CREDENTIAL
+export LOGGINGLEVELOVERRIDE=ERROR
+export PIPELINEPROJECTSDIR=/dbfs/FileStore/pipelineProjects/
+
+pip install "$WHEELDIR$MYCELIUM"
